41527da964ec271ce07d590f761d168691ea37dd
researcher2312 <researcher2312@gmail.com>
2024-04-07 13:29:26 +0200

    removed async dbus reading functions

--- inc/receiver.h
+++ inc/receiver.h
@@ -1,55 +1,9 @@
 #pragma once
 
-#include <vector>
-#include <string>
-#include <memory>
 #include <sdbus-c++/sdbus-c++.h>
 #include "windows.h"
 #include "network_proxy.h"
 
-namespace DBUSAddress {
-    const std::string wireless_enabled {"WirelessEnabled"};
-    const std::string ssid {"Driver"};
-    const std::string service_networkmanager {"org.freedesktop.NetworkManager"};
-    const std::string service_device {"org.freedesktop.NetworkManager.Device"};
-    const std::string networkmanager_path {"/org/freedesktop/NetworkManager"};
-}
-
-
-template<class T>
-class DBUSRequest {
-public:
-    DBUSRequest(std::shared_ptr<sdbus::IProxy> proxy, std::string interface_name, std::string property_name);
-    DBUSRequest() = default;
-    T get_value();
-private:
-    bool future_finished();
-    void set_future();
-    std::string m_interface_name;
-    std::string m_property_name;
-    std::future<sdbus::Variant> m_call_result;
-    std::shared_ptr<sdbus::IProxy> m_proxy;
-    T result;
-};
-
-class DeviceDBUSProxy {
-public:
-    DeviceDBUSProxy();
-    std::string ssid();
-
-private:
-    DBUSRequest<std::string> m_ssid;
-    std::shared_ptr<sdbus::IProxy> m_dbus_proxy;
-};
-
-class NetworkManagerDBUSProxy {
-public:
-    NetworkManagerDBUSProxy();
-    bool wireless_is_enabled();
-private:
-    DBUSRequest<bool> m_wireless_enabled;
-    std::shared_ptr<sdbus::IProxy> m_dbus_proxy;
-};
 
 class NetworkReceiver: public UIWidget {
 public:
--- src/receiver.cpp
+++ src/receiver.cpp
@@ -1,76 +1,7 @@
-#include <iostream>
-#include <chrono>
+#include <string>
 #include "imgui.h"
 #include "receiver.h"
 
-DeviceDBUSProxy::DeviceDBUSProxy()
-{
-    //m_dbus_proxy = sdbus::createProxy(DBUSAddress::service_networkmanager, DBUSAddress::device_path);
-    m_ssid = DBUSRequest<std::string>(m_dbus_proxy, DBUSAddress::service_device, DBUSAddress::ssid);
-}
-
-NetworkManagerDBUSProxy::NetworkManagerDBUSProxy()
-{
-    m_dbus_proxy = sdbus::createProxy(DBUSAddress::service_networkmanager, DBUSAddress::networkmanager_path);
-    m_wireless_enabled = DBUSRequest<bool>(m_dbus_proxy, DBUSAddress::service_networkmanager, DBUSAddress::wireless_enabled);
-}
-
-bool NetworkManagerDBUSProxy::wireless_is_enabled()
-{
-    return m_wireless_enabled.get_value();
-}
-
-std::string DeviceDBUSProxy::ssid()
-{
-    return m_ssid.get_value();
-}
-
-template<class T>
-DBUSRequest<T>::DBUSRequest(std::shared_ptr<sdbus::IProxy> proxy, std::string interface_name, std::string property_name)
-{
-    m_proxy = proxy;
-    m_interface_name = interface_name;
-    m_property_name = property_name;
-}
-
-template<class T>
-bool DBUSRequest<T>::future_finished()
-{
-    if (m_call_result.wait_for(std::chrono::seconds(0)) == std::future_status::ready)
-        return true;
-    else
-        return false;
-}
-
-template<class T>
-void DBUSRequest<T>::set_future()
-{
-    m_call_result = m_proxy->getPropertyAsync(m_property_name)
-                                    .onInterface(m_interface_name)
-                                    .getResultAsFuture();
-    
-}
-
-template<class T>
-T DBUSRequest<T>::get_value()
-{   
-    try {
-        if (m_call_result.valid()) {
-            if (future_finished()) {
-                result = m_call_result.get().get<T>();
-                set_future();
-            }
-        }
-        else {
-            set_future();
-        }
-    }
-    catch (sdbus::Error& error) {
-        std::cerr << "ERR: " << error.getName() << " with message " << error.getMessage() << std::endl;
-    }
-
-    return result;
-}
 
 void NetworkReceiver::display()
 {
@@ -83,5 +14,4 @@ void NetworkReceiver::display()
         ImGui::Text("Wireless connected");
     else
         ImGui::Text("Wireless not connected");
-    // network_proxy.register_wireless_devices();
 }
